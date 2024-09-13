db89d11f3997641788ff55db58c1cef375744a6c
tanmaycodess <97839983+tanmaycodess@users.noreply.github.com>
2024-09-13 16:57:42 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -14,8 +14,10 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+
+
 app.use(cors({
-    origin: 'https://talent-portal.onrender.com', 
+    origin: ['https://talent-portal.onrender.com' ,' https://talent-portal-seven.vercel.app '], 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
