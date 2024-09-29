e3a6f83895d8321b01aa3d9487f12edb201eb0b1
tanmaycodess <97839983+tanmaycodess@users.noreply.github.com>
2024-09-29 09:44:02 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -16,13 +16,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://talent-portal-seven.vercel.app");
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-});
-
 const uri = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 5000;
 
